8cd1525c7212f73813952b5d26f14535f6f22071
maazihad <maazihadin@gmail.com>
2023-06-11 15:26:17 +0600

    all infor collection route created.

--- index.js
+++ index.js
@@ -27,22 +27,108 @@ const client = new MongoClient(uri, {
    },
 });
 
+
+
+//======>>>>>>>>>>>>>>>Validate JWT<<<<<<<<<<<<<<<<<<<<<<<<<
+const verifyJWT = (req, res, next) => {
+   const authorization = req.headers.authorization;
+   if (!authorization) {
+      return res.status(403).send({ error: true, message: "Forbidden Access" });
+   }
+   const token = authorization.split(' ')[1];
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+         return res.status(401).send({ error: true, message: "Unauthorized Access" });
+      }
+      req.decoded = decoded;
+      next();
+   });
+};
+
 async function run() {
    try {
-      const usersCollection = client.db('roosuDb').collection('users');
+      // ================>>>Collections<<<===================
+      const infoCollections = client.db('raosuDb').collection('allInfo');
 
-      app.post('/users', async (req, res) => {
-         const user = req.body;
-         const query = {
-            email: user.email
-         };
-         const existingUser = await usersCollection.findOne(query);
-         if (existingUser) {
-            return res.send({ message: 'user already exists' });
-         }
-         const result = await usersCollection.insertOne(user);
+
+      // ================>>>generate JWT<<<===================
+      // app.post('/jwt', (req, res) => {
+      //    const email = req.body;
+      //    const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15d' });
+      //    // console.log(token);
+      //    res.send({ token });
+      // });
+      // ================>>>Admin Verify<<<===================
+      // const verifyAdmin = async (req, res, next) => {
+      //    const email = req.decoded.email;
+      //    const query = { email: email };
+      //    const user = await allUserCollections.findOne(query);
+      //    if (user?.role !== 'admin') {
+      //       return res.status(403).send({ error: true, message: 'Access Deny' });
+      //    }
+      //    next();
+      // };
+
+      app.get('/sliders', async (req, res) => {
+         const result = await sliderCollections.find({}).toArray();
          res.send(result);
       });
+      app.get('/allInfo', async (req, res) => {
+         const result = await infoCollections.find({}).toArray();
+         res.send(result);
+      });
+
+      // ================>>>Get Instructors<<<===================
+      app.get('/instructors', async (req, res) => {
+         const filter = {};
+         const options = {
+            projection: {
+               instructorName: 1,
+               instructorImage: 1,
+               email: 1
+            },
+         };
+         const result = await infoCollections.find(filter, options).toArray();
+         res.send(result);
+      });
+
+
+      /* 
+         =====================================================
+                        >>>Users API's<<<
+         =====================================================
+      */
+
+      // ================>>>Get All Users<<<===================
+      // app.get("/allUsers", verifyJWT, verifyAdmin, async (req, res) => {
+      //    const result = await allUserCollections.find().toArray();
+      //    res.send(result);
+      // });
+      // ================>>>check is admin ?<<<===================
+      // app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      //    const email = req.params.email;
+      //    if (req.decoded.email !== email) {
+      //       return res.send({ admin: false });
+      //    }
+      //    const query = { email: email };
+      //    const user = await usersCollection.findOne(query);
+      //    const result = { admin: user?.role === 'admin' };
+      //    res.send(result);
+      // });
+      // ================>>>Storing Users In Database<<<===================
+      // app.post('/allUsers', async (req, res) => {
+      //    const user = req.body;
+      //    const query = {
+      //       email: user.email
+      //    };
+      //    const existingUser = await allUserCollections.findOne(query);
+      //    if (existingUser) {
+      //       return res.send({ message: 'user already exists' });
+      //    }
+      //    const result = await allUserCollections.insertOne(user);
+      //    res.send(result);
+      // });
+
 
       // Send a ping to confirm a successful connection
       await client.db('admin').command({ ping: 1 });
@@ -57,9 +143,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-   res.send('AirCNC Server is running..');
+   res.send('Raosu summer camp photography School is running...............');
 });
 
 app.listen(port, () => {
-   console.log(`Raosu summer photography school is running on ${port}`);
+   console.log(`Raosu summer camp photography school is running on ${port}`);
 });
