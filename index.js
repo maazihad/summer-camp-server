5c1deeecc92c61f627a1c769bd1bc038fb023b82
maazihad <maazihadin@gmail.com>
2023-06-13 16:59:44 +0600

    payment intent added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 
-const app = express();
 
 const port = process.env.PORT || 5999;
 
@@ -53,7 +54,11 @@ async function run() {
       // ================>>>Collections<<<===================
       const infoCollections = client.db('raosuDb').collection('allInfo');
       const sliderCollections = client.db('raosuDb').collection('bannerSlider');
-      const allUserCollections = client.db('raosuDb').collection('allUsers');
+      const userCollections = client.db('raosuDb').collection('users');
+      const classesCollections = client.db("raosuDb").collection("classes");
+      const paymentCollections = client.db("raosuDb").collection("payments");
+
+
 
       // ================>>>generate JWT<<<===================
       app.post('/jwt', (req, res) => {
@@ -66,7 +71,7 @@ async function run() {
       const verifyAdmin = async (req, res, next) => {
          const email = req.decoded.email;
          const query = { email: email };
-         const user = await allUserCollections.findOne(query);
+         const user = await userCollections.findOne(query);
          if (user?.role !== 'admin') {
             return res.status(403).send({ error: true, message: 'Access Deny' });
          }
@@ -108,17 +113,101 @@ async function run() {
       });
 
 
-      //=================single room details
-      app.get('/class/:email', verifyJWT, async (req, res) => {
-         const email = req.params.email;
-         const decodedEmail = req.decoded.email;
-         if (decodedEmail !== email) {
-            return res.status(403).send({ error: 1, message: "forbidden access" });
-         };
-         const query = { 'host.email': email };
-         const result = await roomsCollection.find(query).toArray();
-         res.send(result);
+
+
+
+      //================payment intent stripe
+      app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+         const { price } = req.body;
+         const amount = parseInt(price * 100);
+         const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "usd",
+            payment_method_types: ["card"]
+         });
+         res.send({
+            clientSecret: paymentIntent.client_secret,
+         });
       });
+
+
+      // app.get('/dashboard/paymentHistory', verifyJWT, (req, res) => {
+
+      //    const {
+      //       transactionId,
+      //       totalPayment,
+      //       paymentDate
+      //    } = req.query;
+
+      //    const filteredPaymentHistory = paymentHistoryData.filter(payment => {
+      //       if (
+      //          (!transactionId || payment.transactionId === Number(transactionId)) &&
+      //          (!totalPayment || payment.totalPayment === Number(totalPayment)) &&
+      //          (!paymentDate || payment.paymentDate === paymentDate)
+      //       ) {
+      //          return true;
+      //       }
+      //       return false;
+      //    });
+
+      //    res.json(filteredPaymentHistory);
+      // });
+
+      // app.get('/dashboard/payment-history', async (req, res) => {
+      //    const email = req.query.email;
+      //    if (!email) {
+      //       res.send([]);
+      //    }
+      //    const query = {
+      //       email: email
+      //    };
+      //    const result = await paymentCollections.find(query).toArray();
+      //    res.send(result);
+      // });
+
+
+
+      app.get('/dashboard/payment-history', async (req, res) => {
+         const email = req.query.email;
+         if (!email) {
+            res.status(400).send('Email is required');
+            return;
+         }
+         const query = {
+            email: email
+         };
+         try {
+            const result = await paymentCollections.find(query).toArray();
+            res.send(result);
+         } catch (error) {
+            res.status(500).send('Error retrieving payment history');
+         }
+      });
+
+      // =================payment related api
+      app.post('/payments', verifyJWT, async (req, res) => {
+         const payment = req.body;
+         const result = await paymentCollections.insertOne(payment);
+         res.send({
+            result
+         });
+      });
+
+
+
+
+
+      // //=================single room details
+      // app.get('/class/:email', verifyJWT, async (req, res) => {
+      //    const email = req.params.email;
+      //    const decodedEmail = req.decoded.email;
+      //    if (decodedEmail !== email) {
+      //       return res.status(403).send({ error: 1, message: "forbidden access" });
+      //    };
+      //    const query = { 'host.email': email };
+      //    const result = await roomsCollection.find(query).toArray();
+      //    res.send(result);
+      // });
       /* 
          =====================================================
                         >>>Users API's<<<
@@ -138,13 +227,13 @@ async function run() {
 
 
       // ================>>>Get All Users<<<===================
-      app.get("/allUsers", verifyJWT, verifyAdmin, async (req, res) => {
-         const result = await allUserCollections.find().toArray();
+      app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+         const result = await userCollections.find().toArray();
          res.send(result);
       });
 
       // save user route=====================================================
-      app.put('/allUsers/:email', async (req, res) => {
+      app.put('/users/:email', async (req, res) => {
          const email = req.params.email;
          console.log(email);
          const user = req.body;
@@ -155,7 +244,7 @@ async function run() {
          const updateDoc = {
             $set: user
          };
-         const result = await allUserCollections.updateOne(query, updateDoc, options);
+         const result = await userCollections.updateOne(query, updateDoc, options);
          // console.log(result);
          res.send(result);
       });
@@ -167,38 +256,38 @@ async function run() {
             return res.send({ admin: false });
          }
          const query = { email: email };
-         const user = await usersCollection.findOne(query);
+         const user = await userCollections.findOne(query);
          const result = { admin: user?.role === 'admin' };
          res.send(result);
       });
       // // ================>>>Storing Users In Database<<<===================
-      // app.post('/allUsers', async (req, res) => {
+      // app.post('/users', async (req, res) => {
       //    const user = req.body;
       //    const query = {
       //       email: user.email
       //    };
-      //    const existingUser = await allUserCollections.findOne(query);
+      //    const existingUser = await userCollections.findOne(query);
       //    if (existingUser) {
       //       return res.send({ message: 'user already exists' });
       //    }
-      //    const result = await allUserCollections.insertOne(user);
+      //    const result = await userCollections.insertOne(user);
       //    res.send(result);
       // });
       // ------------------------------------------------------------------------------------
 
 
-      app.get('/users/admin/:email', verifyJWT, async (req, res) => {
-         const email = req.params.email;
+      // app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      //    const email = req.params.email;
 
-         if (req.decoded.email !== email) {
-            return res.send({ admin: false });
-         }
+      //    if (req.decoded.email !== email) {
+      //       return res.send({ admin: false });
+      //    }
 
-         const query = { email: email };
-         const user = await usersCollection.findOne(query);
-         const result = { admin: user?.role === 'admin' };
-         res.send(result);
-      });
+      //    const query = { email: email };
+      //    const user = await usersCollection.findOne(query);
+      //    const result = { admin: user?.role === 'admin' };
+      //    res.send(result);
+      // });
 
       app.patch('/users/admin/:id', async (req, res) => {
          const id = req.params.id;
@@ -217,7 +306,7 @@ async function run() {
       app.delete("/users/admin/:id", async (req, res) => {
          const id = req.params.id;
          const query = { _id: new ObjectId(id) };
-         const result = await usersCollection.deleteOne(query);
+         const result = await userCollections.deleteOne(query);
          res.send(result);
       });
 
@@ -225,6 +314,73 @@ async function run() {
 
 
 
+      app.get('/classes', verifyJWT, async (req, res) => {
+         const email = req.query.email;
+         if (!email) {
+            res.send([]);
+         }
+         const decodedEmail = req.decoded.email;
+         if (email !== decodedEmail) {
+            return res.status(403).send({ error: true, message: 'Forbidden access' });
+         }
+         const query = { email: email };
+         const result = await classesCollections.find(query).toArray();
+         res.send(result);
+      });
+
+
+      app.post('/classes', async (req, res) => {
+         const item = req.body;
+         console.log(item);
+         const result = await classesCollections.insertOne(item);
+         console.log(result);
+         res.send(result);
+      });
+
+      app.delete('/classes/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) };
+         const result = await classesCollections.deleteOne(query);
+         res.send(result);
+      });
+
+
+
+
+      //====================
+
+      // app.get('/order-stats', async (req, res) => {
+      //    const pipeline = [
+      //       {
+      //          $lookup: {
+      //             from: 'menu',
+      //             localField: 'menuItems',
+      //             foreignField: '_id',
+      //             as: 'menuItemsData'
+      //          }
+      //       },
+      //       {
+      //          $unwind: '$menuItemsData'
+      //       },
+      //       {
+      //          $group: {
+      //             _id: '$menuItemsData.category',
+      //             count: { $sum: 1 },
+      //             total: { $sum: '$menuItemsData.price' }
+      //          }
+      //       },
+      //       {
+      //          $project: {
+      //             category: '$_id',
+      //             count: 1,
+      //             total: { $round: ['$total', 2] },
+      //             _id: 0
+      //          }
+      //       }
+      //    ];
+      //    const result = await paymentCollections.aggregate(pipeline).toArray();
+      //    res.send(result);
+      // });
 
 
       // Send a ping to confirm a successful connection
